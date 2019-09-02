5b12e346559ad26f2de432fb717e4679214b1b72
Jacob Van Silfhout <jmvsilfhout@gmail.com>
2019-09-02 13:56:34 +1200

    Updated

---
+++ BubbleSort/Task1/Program.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1
+{
+    class Program
+    {
+        static int size = 1000000;
+        static void Main(string[] args)
+        {
+            Stopwatch st1 = new Stopwatch();
+            double[] data = new double[size];
+            Console.WriteLine("************** Bubble Sorting Exercise 1 **************\n");
+            Populate(data);
+            //Display(data);
+            st1.Start();
+            double[] sorted = StandardBubbleSort(data);
+            st1.Stop();
+            //Display(sorted);
+            Console.WriteLine($"Standard bubble sort time: {st1.ElapsedMilliseconds}ms");
+            st1.Reset();
+
+            st1.Start();
+            double[] sorted2 = ImprovedBubbleSort(data);
+            st1.Stop();
+            Console.WriteLine($"\nImproved bubble sort time: {st1.ElapsedMilliseconds}ms");
+
+        }
+
+        static void Populate(double[] arr)
+        {
+            Random rnd = new Random();
+            for(int count = 0; count < arr.Length; count++)
+            {
+                arr[count] = Math.Round((rnd.NextDouble() * 100),2);
+            }
+        }
+
+        static void Display<T>(T[] arr)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+                if((i+1)%10 == 0)
+                    Console.WriteLine();
+            }
+        }
+
+        static double[] StandardBubbleSort(double[] arr)
+        {
+            double[] temp = new double[size];
+            Array.Copy(arr, 0, temp, 0, arr.Length);
+            for(int i = 0; i < temp.Length; i++)
+            {
+                for(int j = 0; j < temp.Length-1; j++)
+                {
+                    if(temp[j] > temp[j+1])
+                    {
+                        double swap = temp[j];
+                        temp[j] = temp[j + 1];
+                        temp[j + 1] = swap;
+                    }
+                }
+            }
+            return temp;
+        }
+
+        static double[] ImprovedBubbleSort(double[] arr)
+        {
+            double[] temp = new double[size];
+            Array.Copy(arr, 0, temp, 0, arr.Length);
+            bool check = false;
+            for (int i = 0; i < temp.Length; i++)
+            {
+                for (int j = 0; j < temp.Length - 1 - i; j++)
+                {
+                    check = false;
+                    if (temp[j] > temp[j + 1])
+                    {
+                        double swap = temp[j];
+                        temp[j] = temp[j + 1];
+                        temp[j + 1] = swap;
+                        check = true;
+                    }
+                }
+                if (!check)
+                    break;
+            }
+            return temp;
+        }
+    }
+}
---
+++ BubbleSort/Task2/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Web.Script.Serialization;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string json = new WebClient().DownloadString("https://uinames.com/api/?ext&amount=500");
+            List<Person> data = new JavaScriptSerializer().Deserialize<List<Person>>(json);
+            Stopwatch s1 = new Stopwatch();
+            s1.Start();
+            BubbleSort(data);
+            s1.Stop();
+
+            foreach(Person x in data)
+            {
+                Console.WriteLine($"Name: {x.name} {x.surname}\nAge: {x.age}\n");
+            }
+            Console.WriteLine($"Bubble sort time: {s1.ElapsedMilliseconds}ms");
+        }
+
+        static void BubbleSort(List<Person> lst)
+        {
+            for (int i = 0; i < lst.Count; i++)
+            {
+                /*Internal loop goes through the elements and checks each one*/
+                for (int j = 0; j < lst.Count - 1; j++)
+                {
+                    /*If the current element is larger than the next element, swap them*/
+                    if (lst[j].age > lst[j + 1].age)
+                    {
+                        Person temp = lst[j + 1];
+                        lst[j + 1] = lst[j];
+                        lst[j] = temp;
+                    }
+                }
+            }
+        }
+    }
+
+
+    public class Person
+    {
+        public string name { get; set; }
+        public string surname { get; set; }
+        public string gender { get; set; }
+        public string region { get; set; }
+        public int age { get; set; }
+        public string title { get; set; }
+        public string phone { get; set; }
+        public Birthday birthday { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public Credit_Card credit_card { get; set; }
+        public string photo { get; set; }
+    }
+
+    public class Birthday
+    {
+        public string dmy { get; set; }
+        public string mdy { get; set; }
+        public int raw { get; set; }
+    }
+
+    public class Credit_Card
+    {
+        public string expiration { get; set; }
+        public string number { get; set; }
+        public int pin { get; set; }
+        public int security { get; set; }
+    }
+
+}
